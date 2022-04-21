2d1d71685bb846a72a5dc8e5a56783777e149980
alqusairy <tugasrpl19@gmail.com>
2022-04-21 14:00:59 +0700

    project zizi

--- app/src/main/java/zizi/polbeng/ac/id/fragmentexample1/MainActivity.kt
+++ app/src/main/java/zizi/polbeng/ac/id/fragmentexample1/MainActivity.kt
@@ -6,15 +6,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var fragmentBookDescription: BookDescription
+    lateinit var fragmentBookTitle: BookTitle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentBookTitle = BookTitle()
+        fragmentBookDescription = BookDescription()
+        val fragTransactiion = supportFragmentManager.beginTransaction()
+        fragTransactiion.add(R.id.fragtop, fragmentBookTitle)
+        fragTransactiion.add(R.id.fragbottom, fragmentBookDescription)
+        fragTransactiion.commit()
     }
 
     fun onBookChanged(index:Int) {
-        val frag = fragmentbookdescription
-        if (frag is BookDescription) {
-            frag.changeDescription(index)
-        }
+        fragmentBookDescription.changeDescription(index)
     }
 }
